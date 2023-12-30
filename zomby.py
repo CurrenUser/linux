26dcdae14e8653caae7c1a07c972bd232aeed563
Ed <curent_user@mail.ru>
2023-12-30 15:25:19 +0300

    добавил заверщение

--- zomby.py
+++ zomby.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import os, time
 
 pid = os.fork()
@@ -5,6 +6,8 @@ if pid:
     while(True):
         print(f'i am the parent, child is pis:{pid}')
         time.sleep(120)
+        exit()
 else:
     print(f'i am the child')
     time.sleep(1000)
+    exit()
