c77a554a9cf8835e6ebb810240004e7cec797daf
Ed <curent_user@mail.ru>
2023-12-30 15:33:44 +0300

    генерация ситуаций зомби

--- zomby.py
+++ zomby.py
@@ -5,9 +5,9 @@ pid = os.fork()
 if pid:
     while(True):
         print(f'i am the parent, child is pis:{pid}')
-        time.sleep(120)
+        time.sleep(1000)
         exit()
 else:
     print(f'i am the child')
-    time.sleep(1000)
+    time.sleep(120)
     exit()
